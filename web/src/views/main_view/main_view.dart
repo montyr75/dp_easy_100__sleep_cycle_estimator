2bdbc5418995abfd1d8aff7eece86b4d0dfa2219
Monty Rasmussen <montyr75@gmail.com>
2014-06-10 17:01:57 -0600

    added UI

--- web/src/views/main_view/main_view.dart
+++ web/src/views/main_view/main_view.dart
@@ -2,40 +2,50 @@ library main_view;
 
 import 'dart:html';
 import 'package:polymer/polymer.dart';
-import 'package:polymer_expressions/filter.dart';
-import '../../utils/filters.dart';
+import 'package:intl/intl.dart';
 
 @CustomTag('main-view')
 class MainView extends PolymerElement {
 
   static const CLASS_NAME = "MainView";
 
-  // constants
-  static const String SAMPLE_CONSTANT = "SAMPLE_CONSTANT";
+  // UI output
+  @observable String bedtime1;
+  @observable String bedtime2;
 
-  // strings
-  static const String SAMPLE_STRING = "sample string";
+  DateFormat timeFormatter = new DateFormat("h:mm a");
 
-  // filters and transformers can be referenced as class fields
-  final Transformer asInteger = new StringToInt();
-
-  // non-visual initialization can be done here
   MainView.created() : super.created();
 
-  // other initialization can be done here
   @override void enteredView() {
     super.enteredView();
     print("$CLASS_NAME::enteredView()");
+
+    newAlarmTime(null, null, $['time_inp']);
   }
 
-  // a sample event handler function
-  void eventHandler(Event event, var detail, Element target) {
-    print("$CLASS_NAME::eventHandler()");
+  void newAlarmTime(Event event, var detail, InputElement target) {
+    print("$CLASS_NAME::newAlarmTime()");
+
+    DateTime alarmTime = DateTime.parse("2014-01-01 ${target.value}");
+    List<String> bedtimes = calculateBedtimes(alarmTime).map((DateTime bedtime) => timeFormatter.format(bedtime)).toList(growable: false);
+
+    bedtime1 = bedtimes[0];
+    bedtime2 = bedtimes[1];
   }
 
   void submit(Event event, var detail, Element target) {
     // prevent app reload on <form> submission
     event.preventDefault();
+  }
+
+  List<DateTime> calculateBedtimes(DateTime alarmTime) {
+    List<DateTime> bedtimes = new List<DateTime>(2);
+
+    bedtimes[0] = alarmTime.subtract(new Duration(hours: 9, minutes: 15));
+    bedtimes[1] = alarmTime.subtract(new Duration(hours: 7, minutes: 45));
+
+    return bedtimes;
   }
 }
 
