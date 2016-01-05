3eb622a99feaadb8375970087e9dca0264a2e364
Kasper Peulen <kasperpeulen@gmail.com>
2016-01-05 23:06:15 +0100

    logger as dependency injection

--- lib/model/global.dart
+++ lib/model/global.dart
@@ -3,14 +3,13 @@ library sleep_cycle_estimator.lib.model.global;
 import 'package:logging/logging.dart';
 import 'package:intl/intl.dart' show DateFormat;
 
-// define logger
-final Logger log = new Logger('message_cipher');
-
-void initLog() {
+Logger initLog() {
   DateFormat dateFormatter = new DateFormat("H:m:s.S");
 
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((LogRecord rec) {
     print('${rec.level.name} (${dateFormatter.format(rec.time)}): ${rec.message}');
   });
+
+  return new Logger('message_cipher');
 }
--- web/main.dart
+++ web/main.dart
@@ -1,15 +1,16 @@
 library sleep_cycle_estimator.web.main;
 
-import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
-import 'package:polymer/polymer.dart';
-
+import 'package:angular2/angular2.dart';
 import 'package:angular2/bootstrap.dart';
-import 'package:sleep_cycle_estimator/views/main_app/main_app.dart';
-
+import 'package:logging/logging.dart';
+import 'package:polymer/polymer.dart';
+import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
 import 'package:sleep_cycle_estimator/model/global.dart';
+import 'package:sleep_cycle_estimator/views/main_app/main_app.dart';
 
 main() async {
   initLog();
   await initPolymer();
-  bootstrap(MainApp);
+
+  bootstrap(MainApp, [provide(Logger, useValue: initLog())]);
 }
